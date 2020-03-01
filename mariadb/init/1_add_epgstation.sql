b6774c8c4914a265ea4e0f7bd478cba33c052064
collelog <collelog.cavamin@gmail.com>
2020-03-01 18:16:26 +0900

    root@localhost の追加は \files\run.sh で行うため、SQLファイルから削除

--- mariadb/init/1_add_epgstation.sql
+++ mariadb/init/1_add_epgstation.sql
@@ -1,11 +1,9 @@
 USE mysql;
-FLUSH PRIVILEGES;
-GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED by 'epgstation' WITH GRANT OPTION;
-SET PASSWORD FOR 'root'@'localhost'=PASSWORD('epgstation');
+FLUSH PRIVILEGES ;
 
 DROP DATABASE IF EXISTS test;
 CREATE DATABASE IF NOT EXISTS `epgstation` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
 GRANT ALL ON `epgstation`.* to 'epgstation'@'localhost' IDENTIFIED BY 'epgstation';
 GRANT SELECT ON `epgstation`.* to 'grafana'@'localhost' IDENTIFIED BY 'grafana';
-FLUSH PRIVILEGES;
+FLUSH PRIVILEGES ;
