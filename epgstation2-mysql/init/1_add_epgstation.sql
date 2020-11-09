d15d36951f182eec004b5caab2e6bcae96736243
collelog <collelog.cavamin@gmail.com>
2020-11-09 15:23:04 +0900

    EPGStation2用MySQLの追加

---
+++ epgstation2-mysql/init/1_add_epgstation.sql
@@ -0,0 +1,10 @@
+USE mysql;
+FLUSH PRIVILEGES ;
+
+DROP DATABASE IF EXISTS test;
+CREATE DATABASE IF NOT EXISTS `epgstation` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+
+GRANT ALL ON `epgstation`.* to 'epgstation'@'%' IDENTIFIED BY 'epgstation';
+GRANT ALL ON `epgstation`.* to 'epgstation'@'localhost' IDENTIFIED BY 'epgstation';
+GRANT SELECT ON `epgstation`.* to 'grafana'@'localhost' IDENTIFIED BY 'grafana';
+FLUSH PRIVILEGES ;
